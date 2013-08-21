484ee3ad99d41c76438e01c6f38068cdd5659aec
Michael Schröder <mc.schroeder@gmail.com>
2013-08-21 20:25:58 +0200

    Refactor & fix insertion

--- Control/Concurrent/Map.hs
+++ Control/Concurrent/Map.hs
@@ -36,6 +36,9 @@
 -- little snag: 'makeStableName' is in IO, so we can't use it inside
 -- 'atomicModifyIORef' which takes a pure function. But you know what?
 -- That's why we have 'unsafePerformIO'! *ducks*
+--
+-- TODO:
+-- unordered-containers uses 'reallyUnsafePtrEquality' so we might as well
 
 module Control.Concurrent.Map where
 
@@ -48,10 +51,12 @@ import Data.IORef
 import Data.Word
 import System.IO.Unsafe  -- see [Note: CAS and pointer equality]
 import System.Mem.StableName
+import Text.Printf
 
 -----------------------------------------------------------------------
 
 -- TODO: list of branches in CNode should be an O(1) array of course
+-- TODO: deal with hash collisions
 
 newtype Map    k v = Map (INode k v)
 newtype INode  k v = INode (IORef (CNode k v))
@@ -61,6 +66,7 @@ data    Branch k v = I !(INode k v)
 
 type Bitmap = Word
 type Hash   = Word
+type Level  = Int
 
 hash :: Hashable a => a -> Hash
 hash = fromIntegral . H.hash
@@ -70,35 +76,53 @@ hash = fromIntegral . H.hash
 empty :: IO (Map k v)
 empty = Map <$> INode <$> newIORef (CNode 0 [])
 
-insert :: (Eq k, Hashable k) => k -> v -> Map k v -> IO ()
-insert k0 v0 m@(Map i0) = do success <- go h0 k0 v0 0 i0
-                             unless success $ insert k0 v0 m
-    where
-        h0 = hash k0
-        go h k v lev (INode ref) = do
-            cn@(CNode bmp arr) <- readIORef ref
-            let (flag,pos) = flagpos bmp h lev
-            if bmp .&. flag == 0
-                then do
-                    let arr' = arrayInsert (S k v) pos arr
-                        cn'  = CNode (bmp .|. flag) arr'
-                    compareAndSwap ref cn cn'
-                else case arr !! pos of
-                    I i2    -> go h k v (lev + w) i2
-                    S k2 v2 -> if k == k2
-                        then do
-                            let arr' = arrayUpdate (S k v) pos arr
-                                cn'  = CNode bmp arr'
-                            compareAndSwap ref cn cn'
-                        else do
-                            let bmp2 = fromIntegral (lev + w)
-                                arr2 = [(S k2 v2), (S k v)]
-                            i2 <- INode <$> newIORef (CNode bmp2 arr2)
-                            let arr' = arrayUpdate (I i2) pos arr
-                                cn'  = CNode bmp arr'
-                            compareAndSwap ref cn cn'
 
-        w = 5
+insert :: (Eq k, Hashable k) => k -> v -> Map k v -> IO ()
+insert k v m@(Map inode) =
+    let h = hash k in repeatUntilTrue (iinsert h k v 0 inode)
+
+iinsert :: (Eq k, Hashable k) => Hash -> k -> v -> Level -> INode k v -> IO Bool
+iinsert h k v lev (INode ref) = do
+    cn@(CNode bmp arr) <- readIORef ref
+    let m = mask h lev
+        i = sparseIndex bmp m
+    if bmp .&. m == 0
+        then let arr' = arrayInsert (S k v) i arr
+                 cn'  = CNode (bmp .|. m) arr'
+             in compareAndSwap ref cn cn'
+        else case arr !! i of
+            I inode2 -> iinsert h k v (nextLevel lev) inode2
+            S k2 v2
+                | k == k2 -> do
+                    let arr' = arrayUpdate (S k v) i arr
+                        cn'  = CNode bmp arr'
+                    compareAndSwap ref cn cn'
+                | otherwise -> do
+                    inode2 <- newINode h (S k v) (hash k2) (S k2 v2) (nextLevel lev)
+                    let arr' = arrayUpdate (I inode2) i arr
+                        cn'  = CNode bmp arr'
+                    compareAndSwap ref cn cn'
+
+newINode :: Hash -> Branch k v -> Hash -> Branch k v -> Int -> IO (INode k v)
+newINode h1 b1 h2 b2 lev = do
+    let i1 = index h1 lev
+        i2 = index h2 lev
+        bmp = (unsafeShiftL 1 i1) .|. (unsafeShiftL 1 i2)
+    ref <- case compare i1 i2 of
+        LT -> newIORef $ CNode bmp [b1,b2]
+        GT -> newIORef $ CNode bmp [b2,b1]
+        EQ -> do inode' <- newINode h1 b1 h2 b2 (nextLevel lev)
+                 newIORef $ CNode bmp [I inode']
+    return (INode ref)
+
+
+repeatUntilTrue :: Monad m => m Bool -> m ()
+repeatUntilTrue act = do
+    success <- act
+    if success
+        then return ()
+        else repeatUntilTrue act
+{-# INLINE repeatUntilTrue #-}
 
 -----------------------------------------------------------------------
 
@@ -116,12 +140,31 @@ compareAndSwap ref old new =
 
 -----------------------------------------------------------------------
 
-flagpos :: Bitmap -> Hash -> Int -> (Word,Int)
-flagpos bmp hc lev = (flag,pos)
-    where
-        flag = (hc `shiftR` (k * lev)) .&. ((1 `shiftL` k) - 1)
-        pos = popCount ((flag - 1) .&. bmp)
-        k = 5
+bitsPerSubkey :: Int
+bitsPerSubkey = floor . logBase 2 . fromIntegral . bitSize $ (undefined :: Word)
+
+subkeyMask :: Bitmap
+subkeyMask = 1 `unsafeShiftL` bitsPerSubkey - 1
+
+index :: Hash -> Level -> Int
+index h lev = fromIntegral $ (h `unsafeShiftR` lev) .&. subkeyMask
+{-# INLINE index #-}
+
+-- when or-ed with a CNode bitmap, determines if the hash is present
+-- in the array at the given level of the trie
+mask :: Hash -> Level -> Bitmap
+mask h lev = 1 `unsafeShiftL` index h lev
+{-# INLINE mask #-}
+
+-- position in the CNode array
+sparseIndex :: Bitmap -> Bitmap -> Int
+sparseIndex bmp m = popCount ((m - 1) .&. bmp)
+{-# INLINE sparseIndex #-}
+
+nextLevel :: Level -> Level
+nextLevel = (+) bitsPerSubkey
+{-# INLINE nextLevel #-}
+
 
 -----------------------------------------------------------------------
 
@@ -132,9 +175,8 @@ arrayInsert x n xs = ys ++ [x] ++ zs
 
 arrayUpdate :: a -> Int -> [a] -> [a]
 arrayUpdate x n xs = ys ++ [x] ++ zs
-    where
-        (ys',zs) = splitAt n xs
-        ys = if n == 0 then [] else tail ys'
+    where (ys,_:zs) = splitAt n xs
+
 
 -----------------------------------------------------------------------
 
